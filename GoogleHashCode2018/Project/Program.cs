b5b88a7c707fbf4afac829fcfdebb2e7d541025a
Eduardo Cáceres <ecaceres@gmv.com>
2018-03-02 12:25:31 +0100

    Apply mentioned changes to last 25% of steps simulations of e, improving 600.000 points 44.320.369

--- GoogleHashCode2018/Project/Program.cs
+++ GoogleHashCode2018/Project/Program.cs
@@ -10,14 +10,14 @@
             //Manager b_easy = new Manager("b_should_be_easy");
             //b_easy.Run();
 
-            Manager c_no_hurry = new Manager("c_no_hurry");
-            c_no_hurry.Run();
+            //Manager c_no_hurry = new Manager("c_no_hurry");
+            //c_no_hurry.Run();
 
             //Manager d_metropolis = new Manager("d_metropolis");
             //d_metropolis.Run();
 
-            //Manager e_high_bonus = new Manager("e_high_bonus");
-            //e_high_bonus.Run();
+            Manager e_high_bonus = new Manager("e_high_bonus");
+            e_high_bonus.Run();
         }
     }
 }
