c144488808ebf275d0ff0b820f87bc649d4279e0
Eduardo Cáceres <ecaceres@gmv.com>
2018-03-02 02:32:54 +0100

    Try a bonus-ride before random one
    Performance improvement to be able to run d & e with new logic.
    35.916.903

--- GoogleHashCode2018/Project/Manager.cs
+++ GoogleHashCode2018/Project/Manager.cs
@@ -16,8 +16,11 @@ namespace Project
 
         private readonly string _inputFileName = "e_high_bonus";
 
-        public Manager()
+        public Manager(string inputFileName = null)
         {
+            if (inputFileName != null)
+                _inputFileName = inputFileName;
+
             string _outputFileName = "output_" + _inputFileName + ".out";
             _inputFileName += ".in";
             _inputPath += _inputFileName;
@@ -40,10 +43,12 @@ namespace Project
 
             IParsedLine firstLine = _file.NextLine();
 
-            long gridRow = firstLine.NextElement<long>();
-            long gridCol = firstLine.NextElement<long>();
+            /*long gridRow = */
+            firstLine.NextElement<long>();
+            /*long gridCol = */
+            firstLine.NextElement<long>();
 
-            Grid = new Grid(gridRow, gridCol);
+            //Grid = new Grid(gridRow, gridCol);
 
             long n_vehicles = firstLine.NextElement<long>();
 
@@ -61,7 +66,6 @@ namespace Project
 
                 long row = line.NextElement<long>();
                 long col = line.NextElement<long>();
-
 
                 long endrow = line.NextElement<long>();
                 long endcol = line.NextElement<long>();
@@ -84,7 +88,7 @@ namespace Project
             if (n_rides != RideList.Count)
                 throw new ParsingException();
 
-            RideList = RideList.OrderBy(ride => ride.Distance).ToList();
+            RideList = RideList.OrderBy(ride => ride.Distance).ToList();    // Room for improvement
         }
 
         private void PrintData()
@@ -98,7 +102,7 @@ namespace Project
 
                 line += VehicleList[i].SuccessfullRides.Count.ToString() + " ";
 
-                VehicleList[i].SuccessfullRides.OrderBy(r => r.RealStart);
+                VehicleList[i].SuccessfullRides = VehicleList[i].SuccessfullRides.OrderBy(r => r.RealStart).ToList();
 
                 foreach (var ride in VehicleList[i].SuccessfullRides)
                 {
@@ -151,21 +155,28 @@ namespace Project
 
                 //VehicleList.OrderBy(v => v.CalculateDistanceToAPoint(RideList.First().InitialPosition));
                 Random rnd = new Random();
+                Ride optimalRide = RideList.FirstOrDefault(r => r.EarlyStart == currentStep);
                 foreach (Vehicle v in VehicleList.Where(v => v.Free).ToList())
                 {
-                    long remainingRides = RideList.Count;
-                    if (remainingRides == 0)
+                    if (RideList.Count == 0)
                         break;
-                    Ride ride = RideList[rnd.Next(0, RideList.Count - 1)];  // Room for improvement
-                    if(ride != null && true == ride.IsOnTimeOrAfterEarlyStart(currentStep + v.CalculateDistanceToAPoint(ride.InitialPosition)))
+
+                    // Since optimalRide is declared before entering the foreach, null comprobation is not enough and .Done needs to be used
+                    Ride existingOptimal = null;
+
+                    if (optimalRide != null && optimalRide.Done == false)
+                        existingOptimal = optimalRide;
+
+                    Ride ride = existingOptimal ?? RideList[rnd.Next(0, RideList.Count - 1)];  // Room for improvement
+                    if (ride != null && true == ride.IsOnTimeOrAfterEarlyStart(currentStep + v.CalculateDistanceToAPoint(ride.InitialPosition)))
                     {
+                        RideList.Remove(ride);
                         ride.Distance += v.CalculateDistanceToAPoint(ride.InitialPosition);
                         ride.Done = true;
                         ride.DoneInEarlyStart = currentStep == ride.EarlyStart;     // Room for improvement
                         v.Free = false;
                         v.StepWhenWillBeFee = currentStep + ride.Distance;  // Including trip to position
 
-                        RideList.Remove(ride);
                         v.SuccessfullRides.Add(ride);
                     }
                 }
